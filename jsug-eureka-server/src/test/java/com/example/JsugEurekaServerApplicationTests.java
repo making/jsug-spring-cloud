77d62e4a1698ae1cca82b47da1cf15e46c35347b
making <makingx@gmail.com>
2016-02-01 14:55:16 +0900

    Add stand along eureka-server

---
+++ jsug-eureka-server/src/test/java/com/example/JsugEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = JsugEurekaServerApplication.class)
+public class JsugEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
