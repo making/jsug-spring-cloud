8b062659ef359f4761a8c667a33638527fb26fab
making <makingx@gmail.com>
2016-02-01 15:13:27 +0900

    Change to use Discovery Client

--- jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
+++ jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
@@ -1,26 +1,34 @@
 package com.example;
 
-import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RestController
 public class JsugConsumerApplication {
 	@Autowired
-	EurekaClient eurekaClient;
+	DiscoveryClient discoveryClient;
 
 	@RequestMapping(path = "/")
 	String hello() {
-		String url = eurekaClient.getNextServerFromEureka("jsug-producer", false).getHomePageUrl();
-		String res = new RestTemplate().getForObject(url, String.class);
-		return res + " from " + url;
+		List<ServiceInstance> instances = discoveryClient.getInstances("jsug-producer");
+		if (instances.isEmpty()) {
+			return "No Service Registry!";
+		}
+		URI uri = instances.get(0).getUri();
+		String res = new RestTemplate().getForObject(uri, String.class);
+		return res + " from " + uri;
 	}
 
 	public static void main(String[] args) {
