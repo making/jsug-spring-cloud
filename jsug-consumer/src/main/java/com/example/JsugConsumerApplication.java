bc19cce7cd24fefe3f2ee4a2c0a89adc3f6be522
making <makingx@gmail.com>
2016-02-01 15:34:59 +0900

    Add Zuul

--- jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
+++ jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
+@EnableZuulProxy
 @RestController
 public class JsugConsumerApplication {
     @Autowired
