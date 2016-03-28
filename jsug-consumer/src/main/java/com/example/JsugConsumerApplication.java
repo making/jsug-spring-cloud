f7ea4db08acb89c6c3ccdddcb99e4bdc6b08abf7
making <makingx@gmail.com>
2016-03-28 17:51:19 +0900

    Add @LoadBalanced

--- jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
+++ jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,12 @@ public class JsugConsumerApplication {
     String hello() {
         String res = demoService.getResponse();
         return res + " from producer";
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
