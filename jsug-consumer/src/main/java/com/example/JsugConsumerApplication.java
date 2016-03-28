5d75fc104e1dd54b67dc6063fd8bf8e63bd7b810
making <makingx@gmail.com>
2016-03-28 17:51:19 +0900

    Add @LoadBalanced
    (cherry picked from commit f7ea4db08acb89c6c3ccdddcb99e4bdc6b08abf7)
    
    # Conflicts:
    #	jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
    
    (cherry picked from commit 7bf603ebd78d2e23cdf174877a096c7c9d43754f)

--- jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
+++ jsug-consumer/src/main/java/com/example/JsugConsumerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +21,12 @@ public class JsugConsumerApplication {
     String hello() {
         String res = restTemplate.getForObject("http://jsug-producer", String.class);
         return res + " from producer";
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     public static void main(String[] args) {
